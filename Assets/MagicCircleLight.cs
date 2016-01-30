574384d7044a60dfa07ffb89a5b466efc8599e4a
林遼平 <c06955960c@gmail.com>
2016-01-30 11:28:12 +0900

    ステージ生成、キャラクターの仮配置、魔法陣の生成、カメラ

---
+++ Assets/MagicCircleLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicCircleLight : MonoBehaviour {
+    public float LightState;
+    public MeshRenderer Body;
+    public MeshRenderer Side1;
+    public MeshRenderer Side2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetLight();
+    }
+
+    void SetLight()
+    {
+        Color _setColor = Color.white;
+        _setColor.a = LightState;
+        Body.materials[1].SetColor("_TintColor", _setColor);
+        Side1.material.SetColor("_TintColor", _setColor);
+        Side2.material.SetColor("_TintColor", _setColor);
+    }
+}
---
+++ Assets/SandBox/Hayashi/Scritps/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	// スマートフォン側.
+	// キャラクターを追いかけるカメラコントローラー.
+
+	// ターゲット.(プレイヤーキャラクター)
+	public Transform target;
+	// キャラクターとのオフセット.
+	private Vector3 offset;
+
+	void Start() {
+		// GameManagerさん,私は誰のカメラなのでしょうか!
+
+		// 決め打ちすみません.
+		offset = new Vector3 (0.0f, -7.0f, 3.5f);
+		transform.rotation = Quaternion.Euler (70.0f, 0.0f, 0.0f);
+	}
+
+	void Update() {
+		// これがUpdateにあるのもどうかと考えてます.
+		// 十字キーのアクションがあった時に一緒に動くようにしてもらうとか？
+		transform.position = target.position - offset;
+	}
+}
