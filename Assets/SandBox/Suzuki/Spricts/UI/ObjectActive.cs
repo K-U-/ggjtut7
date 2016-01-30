cd2e5b9b8e14ca564734364c48716fa79b702b7d
SuzukiTakumaEW <finalfantasy0712kuria@gmail.com>
2016-01-30 16:36:25 +0900

    Add cs

---
+++ Assets/SandBox/Suzuki/Spricts/UI/ObjectActive.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectActive : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] mTargetIsEnable;
+
+    public void Enable()
+    {
+        foreach (var i in mTargetIsEnable) i.SetActive(true);
+    }
+}
