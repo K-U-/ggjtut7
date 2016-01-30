7866b3d49c47a8ec8edc6578cd0eabf369a64078
SuzukiTakumaEW <finalfantasy0712kuria@gmail.com>
2016-01-30 17:20:28 +0900

    Title時のバグの修正 現在はホストとクライアントはレディーを押すと同じシーンを繰り返します。(次のシーンが上がれば対応)

--- Assets/SandBox/Suzuki/Spricts/SceneChange.cs
+++ Assets/SandBox/Suzuki/Spricts/SceneChange.cs
@@ -7,6 +7,7 @@ public class SceneChange : MonoBehaviour {
     public void Change()
     {
         Debug.Log("SceneChange");
+        UnityEngine.Debug.Log("次の読込先シーン : "+name);
         CameraFade.StartAlphaFade(Color.white, false, 0.5f, 0f, () => { Application.LoadLevel(name); });
     }
 }
