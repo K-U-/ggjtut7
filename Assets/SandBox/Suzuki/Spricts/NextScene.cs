7dbc049dde021254351a87aadb6ab43b839d389f
SuzukiTakumaEW <finalfantasy0712kuria@gmail.com>
2016-01-31 15:09:23 +0900

    リザルト製作中

---
+++ Assets/SandBox/Suzuki/Spricts/NextScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class NextScene : MonoBehaviour {
+
+    [SerializeField]
+    private string mSceneName;
+
+    public void SceneChange()
+    {
+        GameManager.GetInstance().LoadScene(mSceneName);
+        GetComponent<Button>().interactable = false;
+    }
+}
